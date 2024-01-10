399e2387f3fac739fdb425b0b2542dda7cec2bcc
hyk06 <hyk@pluxity.com>
2024-01-10 15:18:22 +0900

    파일 정리

--- bmask.py
+++ bmask.py
@@ -3,16 +3,17 @@ import numpy as np
 from pathlib import Path
 import os
 from genericpath import isdir
+import bgremove
+
+result_mask_path = Path(bgremove.result_pre_path/'mask') #누끼이미지 마스크 파일 폴더 생성
 
 def bmask_make():
-    image_path = Path('C:\\Users\\user\\Desktop\\bg_test_out\\images') #누끼이미지 저장 위치
-    result_path1 = Path(image_path.parent/'masks') #mask파일 저장 폴더 생성
-    if os.path.isdir(result_path1)==False:
-        os.mkdir(result_path1)
+    if os.path.isdir(result_mask_path)==False:
+        os.mkdir(result_mask_path)
 
     for file in image_path.glob('*.png'):
         or_image = cv2.imread(str(file))
-        output_path = str(result_path1 / (file.stem + "_mask.png"))
+        output_path = str(result_mask_path / (file.stem + "_mask.png"))
 
         gray = cv2.cvtColor(or_image, cv2.COLOR_BGR2GRAY) #누끼이미지 색상을 그레이로 변경
         ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY) #물체는 검은색으로, 배경은 흰색으로 변경
