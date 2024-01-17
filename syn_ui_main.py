5d46272a0b20dbda8976b8b39aec3657b276da39
hyk06 <hyk@pluxity.com>
2024-01-17 14:54:07 +0900

    obj gen 완료

--- syn_ui_main.py
+++ syn_ui_main.py
@@ -8,50 +8,39 @@ from PyQt5.QtGui import *
 import tkinter
 from tkinter import filedialog
 from pathlib import Path
-import syn_obj_maker_ui
+import syn_ui_obj_maker
 
 form_class = uic.loadUiType(str("main.ui"))[0]
 
 class MainWindow(QMainWindow, form_class):
 
-    bg_pre_path = str()
+    progressChanged = QtCore.pyqtSignal(int)
 
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
 
         ########## Main Tab ##########
-        self.Background_Directory_B.clicked.connect(self.Select_Background_Path)
-        self.Background_Directory_LE.setReadOnly(True)
-        self.Image_Directory_B.clicked.connect(self.Select_Image_Path)
-        self.Image_Directory_LE.setReadOnly(True)
-        self.Run_B.clicked.connect(self.Obj_Gen_Run)
-
-    def Select_Background_Path(self):
-        root = tkinter.Tk()
-        root.withdraw()
-        
-        path = filedialog.askdirectory(parent=root,initialdir = str("./"),title=str("Please select a Background directory") )
-        #path += str("/")
-        self.Background_Directory_LE.setText(path)
-        # self.bg_pre_path = Path(self.Background_Directory_LE.text())
+        self.Obj_Image_Directory_B.clicked.connect(self.Select_Image_Path)
+        self.Obj_Image_Directory_LE.setReadOnly(True)
+        self.Obj_Run_B.clicked.connect(self.Obj_Gen_Run)
+        self.qth_syn = syn_ui_obj_maker.syn_obj_maker(parent = self)
+        self.progressChanged.connect(self.Obj_ProgressBar.setValue)
     
-
-    def Select_Image_Path(self):
+    def Select_Image_Path(self): #합성물체 이미지 폴더 선택
         root = tkinter.Tk()
         root.withdraw()
         
         path = filedialog.askdirectory(parent=root,initialdir = str("./"),title=str("Please select a Image directory") )
-        #path += str("/")
-        self.Image_Directory_LE.setText(path)
-        self.image_pre_path = Path(self.Image_Directory_LE.text())
+        self.Obj_Image_Directory_LE.setText(path)
+        self.image_pre_path = Path(self.Obj_Image_Directory_LE.text())
        
 
     def Obj_Gen_Run(self):
-        self.syn_obj_cls = syn_obj_maker_ui.syn_obj_maker()
-        self.syn_obj_cls.obj_orig_path = self.image_pre_path
-        self.syn_obj_cls.nukki()
-        self.syn_obj_cls.bmask_make()
+        self.Obj_ProgressBar.reset()
+        self.qth_syn.obj_orig_path = self.image_pre_path
+        self.qth_syn.start()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
